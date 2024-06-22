9ca86c54cad4dbd1a36c02ac5f93fc966af00aba
gokulakannan <goks.cloud19@gmail.com>
2024-06-22 22:54:17 +0530

    Admin panel added
    1. Admin file updated
    2. django debug toolbar added

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . models import Customer, Expense
 
-# Register your models here.
+
+admin.site.site_header = "Expense Tracker"
+admin.site.index_title = "Expense Tracker Administration"
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "first_name", "last_name"]
+    list_editable = ["first_name", "last_name"]
+    list_per_page = 10
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ["customer", "date", "amount", "payment_type"]
+    list_editable = ["amount", "payment_type"]
+    list_per_page = 10
+    list_select_related = ['customer']
--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 # Application definition
 
@@ -37,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'expense'
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
